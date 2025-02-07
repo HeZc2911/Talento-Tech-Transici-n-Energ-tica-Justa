b16965caa82fa0f39d6a800d85fee06460fece78
BEL-205-LW-014 <BEL-205-LW-014@UMD.LOCAL.EDU>
2025-02-06 19:32:01 -0500

    pruebita

---
+++ assets/asset.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+s_consumtion = pd.read_csv("assets\solar_energy_consumtion.csv")
+s_share = pd.read_csv("assets\Solar_Share.csv")
+print(s_share.head())
